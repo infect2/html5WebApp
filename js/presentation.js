a212f10b0371fdf08122793c9be947172c87f5bd
sang seok lim <sangseok.lim@sk.com>
2013-05-01 17:32:33 +0900

    add page handler

--- js/presentation.js
+++ js/presentation.js
@@ -1,16 +1,41 @@
 var activePageId; 
+
 $(document).on("impress:stepenter", function(){
     console.log("impress:stepenter:" + window.location.hash);
     activePageId = window.location.hash.replace("#/","");
     executePageHandler( activePageId );
 })
+
 $(document).on("impress:stepleave", function(){
     console.log("impress:stepleave:" + +window.location.hash);
 })
 
-var pageAnimationHandler = {
-    title1: function( pageId ){},
-    title2: function( pageId ){},
-    tstore30: function( pageId ){},
-    xray: function( pageId ){}
+function executePageHandler( pageName ){
+    var handler = pageAnimationHandler.getHandler( pageName );
+    if( typeof handler === "function" ){
+        handler();
+    }
 }
+
+var pageAnimationHandler = {
+    handlers: [],
+    getHandler: function( name ){
+        return this.handlers[ name ];
+    },
+    setHandler: function( name, handler ){
+        this.handlers[ name ] = handler;
+    },
+    init: function( handlerObj ){
+        for( a in handlerObj ){
+            this.handlers[ a ] = handlerObj[a];
+        }
+    }
+}
+
+pageAnimationHandler.init({
+    tstore30: function(){
+        $('#tstore30 iframe').each( function( index, elem ){
+            $(this).attr( "src", $( this ).attr( 'data-src') );
+        });
+    }
+});
